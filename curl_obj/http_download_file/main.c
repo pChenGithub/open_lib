3ba7992f4e5a7665a34a28e39b449a77e6da7fb5
zhuhuihui <1340044380@qq.com>
2021-06-08 08:31:33 +0800

    xxx

--- curl_obj/http_download_file/main.c
+++ curl_obj/http_download_file/main.c
@@ -1,11 +1,10 @@
-
 #include "http_download_file.h"
 
 int main(int argc, char* argv[] ) {
 
     curl_http_download_file_init();
-    /** 下载一个文件，保存在本地，命名为target.txt */
-    curl_http_download_file("https://image.baidu.com/search/down?tn=download&ipn=dwnl&word=download&ie=utf8&fr=result&url=https%3A%2F%2Fgimg2.baidu.com%2Fimage_search%2Fsrc%3Dhttp%253A%252F%252Fdl.iteye.com%252Fupload%252Fattachment%252F0062%252F1127%252Fa6a8e23b-b993-31eb-9702-71d7aafe9925.jpg%26refer%3Dhttp%253A%252F%252Fdl.iteye.com%26app%3D2002%26size%3Df9999%2C10000%26q%3Da80%26n%3D0%26g%3D0n%26fmt%3Djpeg%3Fsec%3D1625590373%26t%3D2326667fde0b5b70f3973f6d81da6405&thumburl=https%3A%2F%2Fss0.bdstatic.com%2F70cFvHSh_Q1YnxGkpoWK1HF6hhy%2Fit%2Fu%3D119984078%2C1634013610%26fm%3D26%26gp%3D0.jpg", "target.jpg");
+    /** 涓嬭浇涓€涓?鏂囦欢锛屼繚瀛樺湪鏈?鍦帮紝鍛藉悕涓簍arget.txt */
+    curl_http_download_file("https://download.java.net/openjdk/jdk15/ri/openjdk-15+36_linux-x64_bin.tar.gz", "jdk-15.tar.gz");
 
     curl_http_download_file_deinit();
 }
