b4c48168d82d702c76533873a55d226e226134aa
zhuhuihui <1340044380@qq.com>
2021-08-18 14:15:45 +0800

    添加easylogger源代码

--- socket_obj/link/net_link.h
+++ socket_obj/link/net_link.h
@@ -1,7 +1,11 @@
+/**
+ * 获取网络链接状态
+ * */
 #ifndef _NET_LINK_H_
 #define _NET_LINK_H_
+// shell脚本方式获取链接状态
 int get_link_status_shell(const char* ifname);
-/* socket prop 方式获取网络链路状态 */
+// socket prop 方式获取网络链路状态
 int get_link_status(const char* ifname);
 /* 获取/设置 iface ip */
 int get_iface_ip(const char* ifname);
