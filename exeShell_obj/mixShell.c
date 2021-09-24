3688fc9ad7adde26a12c37ae6e4859fa535bec59
chen.peng <404285202@qq.com>
2021-09-25 07:33:57 +0800

    执行命令添加关闭,添加是否获取结果

--- exeShell_obj/mixShell.c
+++ exeShell_obj/mixShell.c
@@ -7,7 +7,7 @@
 
 int exeShell(const char* cmd, char* out, const unsigned int out_len) {
     // 验证参数
-    if (NULL==cmd || NULL==out || out_len==0) {
+    if (NULL==cmd) {
         LOG_E("参数检查不通过\n");
         return -1;
     }
@@ -19,13 +19,24 @@ int exeShell(const char* cmd, char* out, const unsigned int out_len) {
         return -1;
     }
 
+    if (NULL==out || 0==out_len) {
+        LOG_E("不接受返回结果，直接退出\n");
+        pclose(fp);
+        fp = NULL;
+        return 0;
+    }
+
     // 读取执行结果
     // 需要命令里面控制获取结果行数，这里默认只取第一行
     if (NULL==fgets(out, out_len-1, fp)) {
         LOG_E("获取命令执行结果失败\n");
+        pclose(fp);
+        fp = NULL;
         return -1;
     }
 
     // 执行命令成功
+    pclose(fp);
+    fp = NULL;
     return 0;
 }
