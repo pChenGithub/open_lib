de1a77f8edd8f5904371fda6529a4308bafaadeb
pChen <18768590968@163.com>
2022-05-13 17:44:47 +0800

    修改检查网络link接口（ioctr方式）

--- socket_obj/link/net_link.c
+++ socket_obj/link/net_link.c
@@ -2,6 +2,7 @@
  * 获取指定iface的link状态的模块
 */
 #include "net_link.h"
+#include "net_errno.h"
 #include <stdio.h>
 #include <sys/types.h>          /* See NOTES */
 #include <sys/socket.h>
@@ -11,11 +12,6 @@
 #include <unistd.h>
 #include <string.h>
 
-#define LOG_E(tag, format, ...) {fprintf(stderr, format, ##__VA_ARGS__);}
-#define LOG_I(tag, format, ...) {fprintf(stdout, format, ##__VA_ARGS__);}
-#define LOG_D(tag, format, ...) {fprintf(stdout, format, ##__VA_ARGS__);}
-
-#define LOGTAG "get_link"
 #define IFACE_NUM 2
 /* shell 方式获取网络链路状态 */
 int get_link_status_shell(const char* ifname) {
@@ -36,47 +32,45 @@ int get_link_status(const char* ifname) {
     struct ifreq ifrs_buf[IFACE_NUM]; // 预计有 IFACE_NUM 个接口
     size_t i = 0;
     // 检查参数
-    assert(NULL!=ifname);
+    if (NULL==ifname)
+        return -NETERR_CHECK_PARAM;
     // 获取socket，
     socketfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (socketfd<0) {
-        LOG_E(LOGTAG, "socket create failed\n");
-        return -1;
-    }
+    if (socketfd<0)
+        return -NETERR_SOCKET_FAIL;
     // 获取ifconf
     ifc.ifc_len = sizeof(ifrs_buf); // 指定获取ifreq的内存大小
     ifc.ifc_ifcu.ifcu_req = ifrs_buf; // 指定获取ifreq放入的内存地址
     if (ioctl(socketfd, SIOCGIFCONF, &ifc)<0) {
-        LOG_E(LOGTAG, "ioctl system call failed\n");
-        ret = -1;
+        ret = -NETERR_SOCKET_GIFCONF_FAIL;
         goto socket_close_exit;
     }
-    //
+    // 
     if_num = ifc.ifc_len/sizeof(struct ifreq);
     ifcu_req = ifc.ifc_ifcu.ifcu_req;
-    LOG_D(LOGTAG, "get iface num %d\n", if_num);
-    //
+    
+    // 找到指定名称的网络接口
     for (i = 0; i < if_num; i++) {
-        LOG_D(LOGTAG, "iface name %s \n",ifcu_req[i].ifr_ifrn.ifrn_name);
+        printf("iface name %s \n",ifcu_req[i].ifr_ifrn.ifrn_name);
         if (!strncmp(ifcu_req[i].ifr_ifrn.ifrn_name, ifname, strlen(ifname))) {
-            LOG_I(LOGTAG, "get ifname %s prop \n", ifname);
+            printf("get ifname %s prop \n", ifname);
             break;
         }
     }
-    //
+    // 如果没有匹配的，指定的接口未找到
     if (i>=if_num) {
-        LOG_I(LOGTAG, "has no interface:%s \n", ifname);
-        ret = -1;
+        fprintf(stderr, "has no interface:%s \n", ifname);
+        ret = -NETERR_HASNO_NETIFNAME;
         goto socket_close_exit;
     }
     ifcu_req +=i;
     //
     if (!(ifcu_req->ifr_ifru.ifru_flags&IFF_UP)) {
-        ret = 1; // down
+        ret = NET_LINK_DOWN; // down
     } else if (!(ifcu_req->ifr_ifru.ifru_flags&IFF_RUNNING)) {
-        ret = 0; // up
+        ret = NET_LINK_UP; // up
     } else {
-        ret = 2; // unkown
+        ret = NET_LINK_UNKNOW; // unkown
     }
 socket_close_exit:
     close(socketfd);
