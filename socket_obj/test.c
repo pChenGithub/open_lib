86975af8669c92b54242a6101a4e64118158de23
pChen <18768590968@163.com>
2022-05-16 07:57:27 +0800

    修改测试程序

--- socket_obj/test.c
+++ socket_obj/test.c
@@ -1,11 +1,39 @@
 #include <stdio.h>
 #include "link/net_link.h"
+#include "link/net_opt.h"
+
+#define IFNAME "enp0s3"
 
 int main(int argc, char* argv[] ) {
+    char ip[16] = {0};
     printf("test\n");
 
-    int ret = get_link_status("ens33");
+    int ret = get_link_status(IFNAME);
     printf("get link status %d\n", ret);
+
+    ret = set_ip(IFNAME, "172.16.70.183");
+    if (ret<0)
+    {
+        printf("set %s ip fail, ret %d\n", IFNAME, ret);
+    }
+
+    ret = get_ip(IFNAME, ip, 16);
+    if (ret<0)
+    {
+        printf("get %s ip fail, ret %d\n", IFNAME, ret);
+        return -1;
+    }
+    printf("get %s ip ok, ip %s\n", IFNAME, ip);
+
+    ret = get_mask(IFNAME, ip, 16);
+    if (ret<0)
+    {
+        printf("get %s mask fail, ret %d\n", IFNAME, ret);
+        return -1;
+    }
+    printf("get %s mask ok, ip %s\n", IFNAME, ip);
+
+
 
     return 0;
 }
