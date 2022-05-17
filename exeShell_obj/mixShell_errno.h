2c2d7c5d84d9e2ca90bd2f338032709741cfef8c
pChen <18768590968@163.com>
2022-05-17 10:04:48 +0800

    添加文件

---
+++ exeShell_obj/mixShell_errno.h
@@ -0,0 +1,10 @@
+#ifndef __MIXSHELL_ERRNO_H__
+#define __MIXSHELL_ERRNO_H__
+#define MIXSHELLERR_CHECKPARAM      1
+#define MIXSHELLERR_POPEN_FAIL      2   // 执行popen出错
+#define MIXSHELLERR_FGETS_FAIL      3   // 执行fgets失败
+#define MIXSHELLERR_EXESHELL_FAIL   4   // 最终结果，shell结果是失败
+#define MIXSHELLERR_EXESHELL_OK     5   // 最终结果，shell结果是成功
+
+#endif
+
---
+++ socket_obj/link/net_def.h
@@ -0,0 +1,8 @@
+#ifndef __NET_DEF__
+#define __NET_DEF__
+#define IP_STR_MAXLEN 16
+
+#define NET_LINK_UP         0
+#define NET_LINK_DOWN       1
+#define NET_LINK_UNKNOW     3
+#endif
