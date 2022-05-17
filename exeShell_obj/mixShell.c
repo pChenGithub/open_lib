8e39436bee903a2610004a0914b680aeb8c6ca82
pChen <18768590968@163.com>
2022-05-17 10:03:20 +0800

    添加复杂一点的执行命令的接口

--- exeShell_obj/mixShell.c
+++ exeShell_obj/mixShell.c
@@ -1,42 +1,73 @@
 #include "mixShell.h"
+#include "mixShell_errno.h"
 #include <stdio.h>
-
-#define LOG_E(format, ...) fprintf(stderr, format, ##__VA_ARGS__)
-#define LOG_I(format, ...) fprintf(stdout, format, ##__VA_ARGS__)
-#define LOG_D(format, ...) fprintf(stdout, format, ##__VA_ARGS__)
+#include <stdlib.h>
 
 int exeShell(const char* cmd, char* out, const unsigned int out_len) {
+    int ret = 0;
     // 验证参数
-    if (NULL==cmd) {
-        LOG_E("参数检查不通过\n");
-        return -1;
-    }
+    if (NULL==cmd)
+        return -MIXSHELLERR_CHECKPARAM;
 
     // 
     FILE* fp = popen(cmd, "r");
-    if (NULL==fp) {
-        LOG_E("打开管道出错\n");
-        return -1;
-    }
+    if (NULL==fp)
+        return -MIXSHELLERR_POPEN_FAIL;
 
-    if (NULL==out || 0==out_len) {
-        LOG_E("不接受返回结果，直接退出\n");
-        pclose(fp);
-        fp = NULL;
-        return 0;
-    }
+    if (NULL==out || 0==out_len)
+        goto closefd_exit;
 
     // 读取执行结果
     // 需要命令里面控制获取结果行数，这里默认只取第一行
-    if (NULL==fgets(out, out_len-1, fp)) {
-        LOG_E("获取命令执行结果失败\n");
-        pclose(fp);
-        fp = NULL;
-        return -1;
-    }
+    if (NULL==fgets(out, out_len-1, fp))
+        ret = -MIXSHELLERR_FGETS_FAIL;
 
     // 执行命令成功
+
+closefd_exit:
     pclose(fp);
     fp = NULL;
-    return 0;
+    return ret;
 }
+
+#define RETSTR_BUFF_LEN     128
+int exeShellWait(const char* cmd, shell_ret_line handret) {
+    int ret = 0;
+    // 验证参数
+    if (NULL==cmd)
+        return -MIXSHELLERR_CHECKPARAM;
+
+    // 执行失败，注意是执行
+    FILE* fp = popen(cmd, "r");
+    if (NULL==fp)
+        return -MIXSHELLERR_POPEN_FAIL;
+
+    if (NULL==handret)
+        goto closefd_exit;
+
+    char* retstr = calloc(RETSTR_BUFF_LEN, 1);
+    // 读取执行结果
+    // 需要命令里面控制获取结果行数，每次获取最新行
+    while (NULL!=fgets(retstr, RETSTR_BUFF_LEN-1, fp)) {
+        ret = handret(retstr);
+        if (SHELL_RET_ERR==ret) {
+            ret = -MIXSHELLERR_EXESHELL_FAIL;
+            break;
+        } else if (SHELL_RET_OK==ret) {
+            ret = 0;
+            break;
+        }
+    }
+    // 执行命令成功/失败
+    //printf("执行命令结果%d\n", ret);
+
+    free(retstr);
+    retstr = NULL;
+
+closefd_exit:
+    pclose(fp);
+    fp = NULL;
+    return ret;
+}
+
+
--- exeShell_obj/mixShell.h
+++ exeShell_obj/mixShell.h
@@ -1,8 +1,15 @@
 #ifndef __MIXSHELL_H__
 #define __MIXSHELL_H__
+typedef enum {
+    SHELL_RET_OK = 0,
+    SHELL_RET_ERR,
+    SHELL_RET_IGNORE,
+} SHELL_RET_TYPE;
+typedef SHELL_RET_TYPE (*shell_ret_line)(const char* linestr);
 // C代码混合shell编程
 // 执行一个shell命令，并输出命令结果（如果有结果），返回值返回该条命令执行的结果（是否成功/执行状态）
 int exeShell(const char* cmd, char* out, const unsigned int out_len);
+int exeShellWait(const char* cmd, shell_ret_line handret);
 #endif
 
 
