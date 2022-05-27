893022fc0f5ba73ce0cadab870fc3e795dad4f9a
pChen <18768590968@163.com>
2022-05-27 08:48:07 +0800

    添加c++兼容

--- socket_obj/link/net_dhcp.h
+++ socket_obj/link/net_dhcp.h
@@ -1,5 +1,8 @@
 #ifndef __NET_DHCP_H__
 #define __NET_DHCP_H__
+#ifdef __cplusplus
+extern "C" {
+#endif
 typedef enum {
     DHCP_RUNNING = 0,
     DHCP_FAIL_END,
@@ -9,6 +12,9 @@ typedef enum {
 typedef int (*hand_dhcpstatus)(DHCP_STATUS sta);
 int start_dhcp(const char* ifname, int timeouts, hand_dhcpstatus hand);
 int stop_dhcp(const char* ifname);
+#ifdef __cplusplus
+}
+#endif
 #endif
 
 
