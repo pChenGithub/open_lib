263c2474b97f8e1ed02b3285548de37144ba8fb8
zhuhuihui <1340044380@qq.com>
2022-08-02 18:05:39 +0800

    添加c++

--- rand_obj/rand_ops.h
+++ rand_obj/rand_ops.h
@@ -1,11 +1,18 @@
 #ifndef __RAND_OPS_H__
 #define __RAND_OPS_H__
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 // 获取一个随机数，返回int型
 int rand_get(unsigned int seed, unsigned int num);
 // 获取一个随机数，返回long型
 long random_get();
 // 获取一个0～3600的随机数
 int rand_get_0to3600(unsigned int seed, unsigned int num);
+#ifdef __cplusplus
+}
+#endif
 #endif
 
 
