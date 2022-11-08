a4d07133fbe2cd0fc82d239f802db2e0f8ad5f62
zhuhuihui <1340044380@qq.com>
2022-11-08 10:22:57 +0800

    时间接口添加一个接口

--- time/timeopt.c
+++ time/timeopt.c
@@ -1,5 +1,7 @@
 #include "timeopt.h"
 #include <time.h>
+#include <sys/time.h>
+#include <stdio.h>
 
 int getStdTime(char* hour, char* min, char* sec) {
     return 0;
@@ -28,5 +30,35 @@ int getStdDateTime(char dt[6]) {
     dt[4] = tmptm.tm_min;
     dt[5] = tmptm.tm_sec;
 
+    return 0;
+}
+
+int getMsTime(char *ms, int mslen)
+{
+    if (NULL==ms || mslen<=0)
+        return -TIMEOPT_ERR_CHECKPARAM;
+    struct timeval tv;
+    if (0!=gettimeofday(&tv,NULL))
+        return -TIMEOPT_ERR_GETTIMEOFDAY;
+    //printf("xx %d s, %d us\n", tv.tv_sec, tv.tv_usec);
+    long long int tms = 0;
+    tms = tv.tv_sec;
+    tms *= 1000;
+    tms += tv.tv_usec/1000;
+    snprintf(ms, mslen, "%lld", tms);
+    return 0;
+}
+
+int getStdDataTimeStr(char *dt, int dtlen, const char *format)
+{
+    if (NULL==dt || dtlen<=0 || NULL==format)
+        return -TIMEOPT_ERR_CHECKPARAM;
+
+    struct tm tmptm;
+    time_t tnow = time(NULL);
+    if (NULL==localtime_r(&tnow, &tmptm))
+        return -TIMEOPT_ERR_LOCALTIME;
+    if (strftime(dt, dtlen, format, &tmptm)<=0)
+        return -TIMEOPT_ERR_STRFTIME;
     return 0;
 }
--- time/timeopt.h
+++ time/timeopt.h
@@ -5,11 +5,18 @@ extern "C" {
 #endif
 #define TIMEOPT_ERR_CHECKPARAM      1
 #define TIMEOPT_ERR_LOCALTIME       2   // 日历时间格式化本地时间失败
+#define TIMEOPT_ERR_STRFTIME        3   // 格式化输出时间字符串失败
+#define TIMEOPT_ERR_GETTIMEOFDAY    4   // 获取gettimeofday失败
 // https://blog.csdn.net/gerryzhu/article/details/50371255  不同的时间和api讲解
 // 获取年月日时分秒,数字返回(6个字节)
 int getStdTime(char* hour, char* min, char* sec);
 int getStdDate(char* year, char* month, char* day);
 int getStdDateTime(char dt[6]);
+// 获取毫秒,字符串返回
+int getMsTime(char* ms, int mslen);
+// 获取时间,年月日,时分秒,字符串返回,年保留2个字节
+int getStdDataTimeStr(char* dt, int dtlen, const char* format);
+//
 #ifdef __cplusplus
 }
 #endif
