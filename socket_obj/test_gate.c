e078aafd25b76353351099370ba4b677be4111bf
zhuhuihui <1340044380@qq.com>
2023-03-27 14:47:19 +0800

    获取网关使用select设置500ms超时

--- socket_obj/test_gate.c
+++ socket_obj/test_gate.c
@@ -7,7 +7,7 @@ int main(int argc, char const *argv[])
 {
     int ret = 0;
 #if 1
-#if 1
+#if 0
     // 设置网关
     //ret = set_gateway("ens33", "192.168.0.3", "255.255.0.0", "192.168.1.1");
     // 添加默认网关
@@ -32,7 +32,7 @@ int main(int argc, char const *argv[])
     ROUTE_LIST list[2] = {0};
     int len = 2;
     //ret = nl_getGateways("default", NULL, list, &len);
-    ret = nl_getGateways(NULL, NULL, list, &len);
+    ret = nl_getGateways(NULL, NULL, NULL, list, &len);
     //ret = get_gateway(NULL, NULL, 0);
     if (ret<0)
         printf("获取网关失败，错误码%d\n", ret);
