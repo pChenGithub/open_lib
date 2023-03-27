e07a783a3376c55bc934c13d22e4664d9c3108f8
zhuhuihui <1340044380@qq.com>
2023-03-27 14:50:27 +0800

    添加编译脚本

---
+++ curl_obj/build_arm.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+./configure --host=arm-linux CC=/home/zhh/work/yt328_reader/020.源代码/toolchain/gcc-linaro-5.5.0-2017.10-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc --prefix=/home/zhh/work/open_lib/curl_obj/build_328arm --with-openssl=/home/zhh/work/open_lib/openssl/build_328arm --disable-tls-srp --disable-rtsp --disable-hsts --disable-alt-svc --disable-mqtt --disable-ftp --disable-tftp --disable-smb --disable-gopher --disable-imap --disable-pop3 --disable-smtp --disable-telnet --disable-dict
+
+
+
+./configure --prefix=/home/zhh/work/open_lib/curl_obj/build_x86 --with-openssl=/home/zhh/work/open_lib/openssl/build_x86 --disable-tls-srp --disable-rtsp --disable-hsts --disable-alt-svc --disable-mqtt --disable-ftp --disable-tftp --disable-smb --disable-gopher --disable-imap --disable-pop3 --disable-smtp --disable-telnet --disable-dict
+
+
+
+
+
+
+
+./configure --host=arm-linux CC=/home/zhh/work/yt328_reader/020.源代码/toolchain/gcc-linaro-5.5.0-2017.10-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc --prefix=/home/zhh/work/open_lib/curl_obj/build_328armout --with-openssl=/home/zhh/nfs/tmpcurl/ssl --disable-tls-srp --disable-rtsp --disable-hsts --disable-alt-svc --disable-mqtt --disable-ftp --disable-tftp --disable-smb --disable-gopher --disable-imap --disable-pop3 --disable-smtp --disable-telnet --disable-dict
