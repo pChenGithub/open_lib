dc411384f5275f81ba5812f38183e0e85c2a2e1d
chenp <18768590968@163.com>
2024-02-22 15:41:07 +0800

    添加升级文件传输

--- dev_scan/scan_recv.c
+++ dev_scan/scan_recv.c
@@ -1,4 +1,5 @@
 #include "tcpIp/multicast.h"
+#include "tcpIp/socket_file.h"
 #include "protocol/device_prot.h"
 #include <unistd.h>
 #include <stdio.h>
@@ -16,12 +17,19 @@ static void recvmsgbd(handMulticastArg* arg) {
     // 处理广播指令
     switch (cmd->cmd) {
     case  CMD_DEVICE_UPDATE:
+            printf("设备更新命令\n");
+            DEV_CMD_RSP* rsp = (DEV_CMD_RSP*)body->sendBuff;
+            memcpy(&rsp->cmd, cmd, sizeof(DEV_CMD));
+            rsp->code = CMD_RSP_CODE_REDY;
+            // 准备号接收文件,
+            // 回复之后,对方就开始发送文件了
+            multicast_resp(body, (char*)rsp, sizeof(DEV_CMD_RSP));
     break;
     case CMD_DEVICE_SCAN: 
     default:
     {
             // 获取信息,返回
-            printf("发送回复信息\n");
+            printf("扫描设备命令\n");
             DEVICE_INFO* rsp = (DEVICE_INFO*)body->sendBuff;
             fill_device_info(rsp);
             multicast_resp(body, (char*)rsp, sizeof(DEVICE_INFO));
@@ -31,7 +39,12 @@ static void recvmsgbd(handMulticastArg* arg) {
 #endif
 }
 
+static void tcprcvbk(handTcpArg* arg) {
+    printf("收到一包数据\n");
+}
+
 static RECV_MSG_BODY *body = NULL;
+static RECV_TCP_MSG_BODY *tcpbody = NULL;
 
 int main(int argc, char const *argv[]) {
     if (argc<3) {
@@ -48,6 +61,12 @@ int main(int argc, char const *argv[]) {
 		if (ret<0) {
 			printf("监听失败, 错误码 %d\n", ret);
 		}
+    }
+
+    // 启动tcp监听
+    ret = tcp_listen_start(&tcpbody, tcprcvbk, (char*)(argv[1]), 10002);
+    if (ret<0) {
+        printf("监听失败, 错误码 %d\n", ret);
     }
 
     // 启动http监听,一些点对点的服务
